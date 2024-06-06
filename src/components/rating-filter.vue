e3e972e97786dccf3a699b57cba90c2bddee1954
JMC <jymc77@gmail.com>
2024-06-06 23:42:09 +0200

    fixe todo-bonus

--- src/components/rating-filter.vue
+++ src/components/rating-filter.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+defineProps<{
+  results: number
+}>();
+const emits = defineEmits([`filter:rating`]);
+
 const ratingFilterItems = [
   { title: `all restaurants`, value: 0 },
   { title: `5 stars`, value: 5 },
@@ -7,19 +12,27 @@ const ratingFilterItems = [
   { title: `2 stars or more`, value: 2 },
   { title: `1 star or more`, value: 1 },
 ];
+
+const select = ref(ratingFilterItems[0]);
 </script>
 
 <template>
   <VCard variant="outlined">
-    <VAlert type="info">
-      TODO: this is a bonus! <br>
-      Implement the rating filter
-    </VAlert>
     <VCardText>
       <div class="flex flex-wrap items-center justify-between gap-8">
         <strong>Filter the restaurants by ratings</strong>
-        <VSelect :items="ratingFilterItems" variant="solo" hide-details="auto" placeholder="all restaurants" />
+        <VSelect
+          v-model="select"
+          :items="ratingFilterItems"
+          variant="solo"
+          hide-details="auto"
+          placeholder="all restaurants"
+          @update:model-value="emits(`filter:rating`, $event)"
+        />
       </div>
+      <p v-if="results">
+        {{ results }} restaurant(s) found
+      </p>
     </VCardText>
   </VCard>
 </template>
